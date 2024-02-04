933bd1d881f6b632ba492d4e9bb3ccd68c852998
DIR\mark.a.mckenna <mark.a.mckenna.contractor@bbva.com>
2024-02-04 15:51:05 +0100

    Initial Commit

---
+++ src/main/java/io/javabrains/movieinfoservice/models/Movie.java
@@ -0,0 +1,15 @@
+package io.javabrains.movieinfoservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+
+	private String movieId;
+	private String name;
+
+}
